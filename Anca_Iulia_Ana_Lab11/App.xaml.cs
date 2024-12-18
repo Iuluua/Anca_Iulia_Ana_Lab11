67f243026423d8f1fc6586a8d9f7343b8ffe4879
Iulia Anca <iulia.anca@evozon.com>
2024-12-18 15:16:04 +0200

    Add project files.

---
+++ Anca_Iulia_Ana_Lab11/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Anca_Iulia_Ana_Lab11.Data;
+
+namespace Anca_Iulia_Ana_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            //InitializeComponent();
+
+            //MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService()); 
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Anca_Iulia_Ana_Lab11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Anca_Iulia_Ana_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
